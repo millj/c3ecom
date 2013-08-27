c308600aa88f314ea6ed81719e7cd72efff41693
Josh <jmillsy@gmail.com>
2013-08-27 18:37:49 +1000

    finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to c3ecom!"
+      sign_in @user
+      flash[:success] = "Welcome to the c3ecom!"
       redirect_to @user
     else
       render 'new'
