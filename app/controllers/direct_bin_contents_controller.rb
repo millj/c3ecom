5aa99953e7264da2ca4de5815b62705821a33874
Josh <millj@DSS.ccubed>
2013-09-06 12:15:55 +1000

    add bin name to search

--- app/controllers/direct_bin_contents_controller.rb
+++ app/controllers/direct_bin_contents_controller.rb
@@ -39,7 +39,6 @@ class DirectBinContentsController < ApplicationController
   end
 
   def index
- #   @direct_bin_contents = DirectBinContent.paginate(page: params[:page])
     @direct_bin_contents = DirectBinContent.search(params[:search], params[:page])
   end
 
--- app/models/direct_bin_content.rb
+++ app/models/direct_bin_content.rb
@@ -3,15 +3,17 @@ class DirectBinContent < ActiveRecord::Base
   validates :item_code, presence: true, length: {maximum: 15}
 
   default_scope -> { order('bin_name ASC')}
+
   self.per_page = 25
 
 
 
   def self.search(search, page)
     paginate :per_page => 25, :page => page,
-             :conditions => ['item_code like ?', "%#{search}%"],
+             :conditions => ['item_code like ? or bin_name like ?', "%#{search}%", "%#{search}%"],
              :order => 'bin_name, item_code'
   end
+
 
 
 end
