ffd79d4aa7116d814985b648cff9569cb7fb944c
Josh <millj@DSS.ccubed>
2013-10-10 11:42:35 +1100

    find order num and update scanned qty

--- app/controllers/allocation_order_lines_controller.rb
+++ app/controllers/allocation_order_lines_controller.rb
@@ -1,10 +1,29 @@
 class AllocationOrderLinesController < ApplicationController
 
  def allocate_item
-   @basket_number = params[:upc]
+
+   sql_query = 'select a.order_num
+                   from c3rails_dev.allocation_orders a,
+                        c3rails_dev.allocation_order_lines b
+                   where b.order_num = a.order_num
+                     and b.upc = \'' + params[:upc] + '\'
+                     and b.qty_scanned < b.qty_required
+                    order by a.order_priority desc, a.order_date desc limit 1'
+   result = ActiveRecord::Base.connection.select_value(sql_query)
+   @basket_number = result
+
+   sql_query = 'update c3rails_dev.allocation_order_lines b
+                   set b.qty_scanned = b.qty_scanned + 1
+
+                   where b.order_num = \'' + @basket_number + '\'
+                     and b.upc = \'' + params[:upc] + '\'
+                     and b.qty_scanned < b.qty_required'
+   ActiveRecord::Base.connection.execute(sql_query)
 
    render :allocate
-
+ rescue Exception => exc
+   logger.error("Message for the log file #{exc.message}")
+   flash[:notice] = "Store error message #{exc.message}"
  end
 
   def index
