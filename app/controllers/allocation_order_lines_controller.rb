db7f1a02e45d4a13ee29f8d5f4459023aa5a793d
Josh <millj@DSS.ccubed>
2013-10-11 14:23:51 +1100

    more error catching

--- app/controllers/allocation_order_lines_controller.rb
+++ app/controllers/allocation_order_lines_controller.rb
@@ -13,7 +13,7 @@ class AllocationOrderLinesController < ApplicationController
                     order by a.order_priority desc, a.order_date desc limit 1'
    result = ActiveRecord::Base.connection.select_value(sql_query)
 
-   @display_screen = 'basket_display_error'
+
    unless result.nil?
 
      @basket_number = result.split(",").first
@@ -26,10 +26,17 @@ class AllocationOrderLinesController < ApplicationController
                        and b.qty_scanned < b.qty_required'
      ActiveRecord::Base.connection.execute(sql_query)
      @display_screen = 'basket_display'
+   else
+     @display_screen = 'basket_display_error'
+     sql_query = 'select item_code from c3dss.dim_item
+                   where upc = \'' + params[:upc] + '\'
+                 '
+     result = ActiveRecord::Base.connection.select_value(sql_query)
+     @order_number = params[:upc] + ' Item Code: ' + result
    end
 
-
    render :allocate
+
  rescue Exception => exc
    logger.error("Message for the log file #{exc.message}")
    flash[:notice] = "Error message #{exc.message}"
