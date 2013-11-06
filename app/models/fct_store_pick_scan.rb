c638f6564ca4f8c64220eafb5425a70301e0355d
Josh <millj@DSS.ccubed>
2013-11-06 14:04:55 +1100

    edit, delete fct_store_pick_scans

--- app/models/fct_store_pick_scan.rb
+++ app/models/fct_store_pick_scan.rb
@@ -16,7 +16,7 @@ class FctStorePickScan < ActiveRecord::Base
 
   def self.search(search, page)
     paginate :per_page => 25, :page => page,
-             :conditions => ['store_code like ? or wpi_code like ? or wsn_code like ? or picked_by like ? or scanned_by like ?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%"],
+             :conditions => ['store_code like ? or wpi_code like ? or wsn_code like ? or picked_by like ? or scanned_by like ? or checked_by like ?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%"],
              :order => 'store_code, wpi_code'
   end
 
