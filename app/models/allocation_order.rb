c913ec73fcf371614fde4118cbff49e041675923
millj <millj@DSS.ccubed>
2013-12-04 13:48:33 +1100

    order selection screen changes, criteria mods, route for the screen, menu item in header

--- app/models/allocation_order.rb
+++ app/models/allocation_order.rb
@@ -22,15 +22,20 @@ class AllocationOrder < ActiveRecord::Base
 
   def self.order_selection
     sql_query = 'SELECT a.order_num,
-                       c.basket_num, a.order_processed
-                FROM allocation_orders a,
-                     baskets c
-                WHERE NOT EXISTS (SELECT *
-                                  FROM allocation_order_lines b
-                                  WHERE b.order_num = a.order_num
-                                  AND   b.qty_required <> b.qty_scanned)
-                AND   c.order_num = a.order_num
-                AND   a.order_complete = 0'
+                        c.basket_num,
+                        a.order_date,
+                        a.order_priority,
+                        a.order_processed
+                   FROM c3ecom.allocation_orders a,
+                        c3ecom.baskets c
+                   WHERE NOT EXISTS (SELECT *
+                                       FROM c3ecom.allocation_order_lines b
+                                       WHERE b.order_num = a.order_num
+                                         AND   b.qty_required <> b.qty_scanned)
+                     AND c.order_num = a.order_num
+                     AND a.order_complete = 0
+                     and a.order_processed = false
+                   order by a.order_priority, a.order_date'
 
     connection.select_all(sql_query)
   end
