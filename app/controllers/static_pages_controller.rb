2635779cabb0f59c4b27c57032263f167e477aa5
millj <jmillsy@gmail.com>
2014-02-18 11:01:51 +1100

    changed load to uplaod to new staging tables

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -60,6 +60,9 @@ class StaticPagesController < ApplicationController
 
   def load_table_mecca
 
+    sql_query =  'truncate table ' + params[:select_table]
+    ActiveRecord::Base.connection.execute(sql_query)
+
     sql_query =  'load data infile \'/mnt/integration/demand/' + params[:select_table][((params[:select_table].index('.')) + 1)..(params[:select_table].length)] + '.csv \' into table ' + params[:select_table] + ' fields terminated by \',\' ignore 1 lines'
     ActiveRecord::Base.connection.execute(sql_query)
     flash[:success] = 'Loaded ' + params[:select_table]
@@ -72,6 +75,9 @@ class StaticPagesController < ApplicationController
   end
 
   def load_table_km
+
+    sql_query =  'truncate table ' + params[:select_table]
+    ActiveRecord::Base.connection.execute(sql_query)
 
     sql_query =  'load data infile \'/mnt/integration/demand/' + params[:select_table][((params[:select_table].index('.')) + 1)..(params[:select_table].length)] + '.csv \' into table ' + params[:select_table] + ' fields terminated by \',\' ignore 1 lines'
     ActiveRecord::Base.connection.execute(sql_query)
