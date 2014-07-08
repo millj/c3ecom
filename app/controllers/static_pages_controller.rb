e43da18620b0996754c1ce8d921159b450d465cc
millj <jmillsy@gmail.com>
2014-07-08 14:19:46 +1000

    changed c3ecom to Mecca Brands

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -25,6 +25,12 @@ class StaticPagesController < ApplicationController
 
   def select_oms
 
+    # run the xaction to get the list of currently available orders
+    #uri = URI.parse('http://dss.ccubed.local:8084/pentaho/ViewAction')
+    #params = { :solution => 'CFC', :action =>'mbecom_retrieve_available_orders_list.xaction', :path => '', :userid => 'report', :password => 'report' }
+    #uri.query = URI.encode_www_form(params)
+    #res = Net::HTTP.get_response(uri)
+
     # Get orders ready to select
     sql_query1 = 'select *, false as order_selected from mbecom.mb_order_status where order_ecom_status = 1 order by priority desc, order_number'
 
@@ -48,6 +54,7 @@ class StaticPagesController < ApplicationController
       ActiveRecord::Base.connection.execute(sql_query1)
     end
 
+    # Change this xaction to whichever xaction we need to run to get the order header/details
     uri = URI.parse('http://dss.ccubed.local:8084/pentaho/ViewAction')
     params = { :solution => 'CFC', :action =>'mbecom_retrieve_available_orders_list.xaction', :path => '', :userid => 'report', :password => 'report' }
     uri.query = URI.encode_www_form(params)
