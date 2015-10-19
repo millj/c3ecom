11f6375d71a2a4f0c9e8b278cd81ed61ac5a776d
millj <jmillsy@gmail.com>
2015-10-19 16:35:59 +1100

    changed search params and default scope on fct_wms_carton_asn_control

--- app/models/fct_wms_carton_asn_control.rb
+++ app/models/fct_wms_carton_asn_control.rb
@@ -3,10 +3,11 @@ class FctWmsCartonAsnControl < ActiveRecord::Base
   self.table_name = 'c3dss.fct_wms_carton_asn_control'
   self.primary_key = 'sscc'
 
+  default_scope { where ("exported = 0")}
 
   def self.search(search, page)
     paginate :per_page => 25, :page => page,
-             :conditions => ['sscc like ?', "%#{search}%"],
+             :conditions => ['sscc like ? or to_number like ?', "%#{search}%", "%#{search}%"],
              :order => 'sscc'
   end
 end
